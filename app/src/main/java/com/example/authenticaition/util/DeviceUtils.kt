0721d9fb116cc736d25d36e98f83dd2eb7803d86
ankitkumar5049 <72911975+ankitkumar5049@users.noreply.github.com>
2023-06-29 19:55:52 +0530

    different enviornment added

--- app/src/main/java/com/example/authenticaition/util/DeviceUtils.kt
+++ app/src/main/java/com/example/authenticaition/util/DeviceUtils.kt
@@ -2,6 +2,7 @@ package com.example.authenticaition.util
 
 import android.os.Build
 import android.util.Log
+import java.io.File
 
 class DeviceUtils {
     companion object {
@@ -17,18 +18,55 @@ class DeviceUtils {
                     || Build.MODEL.contains("google_sdk")
                     || Build.MODEL.contains("Emulator")
                     || Build.MODEL.contains("Android SDK built for x86")
+                    || Build.MODEL.contains("google_sdk")
+                    || Build.MODEL.contains("VirtualBox")
+                    || Build.MODEL.contains("VMware")
                     || Build.MANUFACTURER.contains("Genymotion")
+                    || Build.MANUFACTURER.contains("Genymotion")
+                    || Build.MANUFACTURER.contains("unknown")
+                    || Build.MANUFACTURER.contains("Andy")
+                    || Build.MANUFACTURER.contains("Andy OS")
+                    || Build.MANUFACTURER.contains("nox")
+                    || Build.MANUFACTURER.contains("noxvm")
+                    || Build.MANUFACTURER.contains("Xamarin")
+                    || Build.MANUFACTURER.contains("VMware")
+                    || Build.MANUFACTURER.contains("Bluestack")
                     || Build.HARDWARE.contains("goldfish")
                     || Build.HARDWARE.contains("ranchu")
                     || Build.HARDWARE.contains("vbox86")
                     || Build.HARDWARE.contains("vbox64")
+                    || Build.HARDWARE.contains("vbox")
+                    || Build.HARDWARE.contains("nox")
+                    || Build.HARDWARE.contains("goldeb")
+                    || Build.PRODUCT.contains("Bluestacks")
                     || Build.PRODUCT.contains("sdk_google")
                     || Build.PRODUCT.contains("google_sdk")
                     || Build.PRODUCT.contains("sdk")
                     || Build.PRODUCT.contains("sdk_x86")
                     || Build.PRODUCT.contains("vbox86p")
                     || Build.PRODUCT.contains("emulator")
-                    || Build.PRODUCT.contains("simulator"))
+                    || Build.PRODUCT.contains("simulator")
+                    // for rooted device
+                    || File("/system/app/Superuser.apk").exists()
+                    || File("/sbin/su").exists()
+                    || File("/system/bin/su").exists()
+                    || File("/system/xbin/su").exists()
+                    || File("/data/local/xbin/su").exists()
+                    || File("/data/local/bin/su").exists()
+                    || File("/system/su").exists()
+                    || File("/system/bin/failsafe/su").exists()
+                    || File("/data/local/su").exists()
+                    || File("/su/bin").exists()
+                    || File("/su/xbin").exists()
+                    || File("/sbin/magisk").exists()
+                    || File("/init.magisk.rc").exists()
+                    || File("/system/bin/.ext/.su").exists()
+                    || File("/system/xbin/mu").exists()
+                    || File("/data/magisk.img").exists()
+                    || File("/data/adb/magisk").exists()
+                    || File("/data/adb/modules/magisk").exists()
+                    || File("/dev/com.koushikdutta.superuser.daemon/").exists()
+                    || File("/dev/su").exists())
         }
 
         fun isGenuineDevice(): Boolean {
